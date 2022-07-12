cec51a9152e562dce34601c303de64dffb753691
anderson.silva <anderson.silva@ecocentauro.com.br>
2022-07-12 15:59:29 -0300

    Curso Alura

---
+++ alura/exibindo o menu/sem-entrada-dados.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+func main() {
+	nome := "Anderson"
+	idade := 31
+	versao := 1.1
+	fmt.Println("Olá Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
