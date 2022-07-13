67d455d71b061cddd4fad7240d9c6a9d143421dc
Anderson <adersoosilvaa@gmail.com>
2022-07-12 21:37:14 -0300

    Curso go alura

---
+++ alura/projeto/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Anderson"
+	versao := 1.1
+
+	fmt.Println("Olá sr(a).", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar o Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O valor da variável é", comando)
+}
